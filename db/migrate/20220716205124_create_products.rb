2d600567d18c22483588f4eab71585b86ce88440
+ <+>
2022-07-16 17:39:45 -0400

    create migrations and models

---
+++ db/migrate/20220716205124_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :productname
+      t.text :description
+      t.boolean :available
+      t.integer :cost
+      t.integer :price
+      t.string :category
+      t.string :img_url
+    end
+  end
+end
